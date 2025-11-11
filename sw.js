98dbd9d27a5d1d5ed9c6f2b9e82fa1978ef664a2
Dexter5000000 <Dexter5000000@users.noreply.github.com>
2025-11-11 03:46:42 +0000

    deploy: 32bb64751b5cd44e62dff7e2a70eedd97cd11324

--- sw.js
+++ sw.js
@@ -1276,7 +1276,7 @@ class x extends v {
   }
 }
 ne();
-ce([{"revision":null,"url":"assets/index-CTv3a_ef.js"},{"revision":null,"url":"assets/index-DqsJzgpS.css"},{"revision":"8f8ac0c6b5453aa6098525ca5e2e5f0e","url":"favicon.svg"},{"revision":"3e6a85d2b8aab42e7138e0a45b4ecc57","url":"index.html"},{"revision":"277642ce3c60a785f29f9651a6c4a474","url":"pwa-192x192.svg"},{"revision":"30bccd65649ffe69fe3aa40d01b2169e","url":"pwa-512x512.svg"},{"revision":"1872c500de691dce40960bb85481de07","url":"registerSW.js"},{"revision":"277642ce3c60a785f29f9651a6c4a474","url":"pwa-192x192.svg"},{"revision":"30bccd65649ffe69fe3aa40d01b2169e","url":"pwa-512x512.svg"},{"revision":"559e3f201b0a16dd66742b2c015f6a11","url":"manifest.webmanifest"}]);
+ce([{"revision":null,"url":"assets/index-BCklHln0.js"},{"revision":null,"url":"assets/index-DqsJzgpS.css"},{"revision":"7e03c1d3775a1b9713b04293cc526911","url":"favicon.svg"},{"revision":"4bebe0fabeaa03424da9dee3e2ee7e9f","url":"index.html"},{"revision":"0ed9bb5f99ac55eb49fad94fe73945ba","url":"pwa-192x192.svg"},{"revision":"e3306abec97f935cba52aaf1a611e010","url":"pwa-512x512.svg"},{"revision":"1872c500de691dce40960bb85481de07","url":"registerSW.js"},{"revision":"0ed9bb5f99ac55eb49fad94fe73945ba","url":"pwa-192x192.svg"},{"revision":"e3306abec97f935cba52aaf1a611e010","url":"pwa-512x512.svg"},{"revision":"559e3f201b0a16dd66742b2c015f6a11","url":"manifest.webmanifest"}]);
 b(
   ({ request: a }) => a.destination === "script" || a.destination === "style" || a.destination === "image",
   new x({
